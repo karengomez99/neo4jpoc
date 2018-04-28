4991fe2cadcde135eba6bae4f7c88b473d5b8ec7
Karen Tatiana Gomez Suarez <karen.gomez@ceiba.com.co>
2018-04-27 22:38:40 -0500

    Imprimir la ruta mas corta en consola, nombre de nodo, relación, nombre de nodo siguiente

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var neo4j = require('neo4j-driver').v1;
+var loop = require('node-while-loop');
 var path1 = __dirname + '/views/';
 
 var app = express();
@@ -24,7 +25,7 @@ var session = driver.session();
 app.get('/', function (req, res) {
     session
         .run('MATCH(n:Concept) RETURN n')
-        .then(function(result) {
+        .then(function (result) {
             var movieArr = [];
             result.records.forEach(function (record) {
                 movieArr.push({
@@ -32,43 +33,27 @@ app.get('/', function (req, res) {
                     released: record._fields[0].properties.released
                 });
             });
-
-            session
-                .run('MATCH(n:Actor) RETURN n')
-                .then(function(result2) {
-                    var personArray = [];
-                    result2.records.forEach(function (record) {
-                        personArray.push({
-                            id: record._fields[0].identity.low,
-                            name: record._fields[0].properties.name
-                        });
-                    });
-                    res.sendFile(path1 + 'poc.html', {
-                        movies: movieArr,
-                        actors: personArray
-                    });
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
+            res.sendFile(path1 + 'poc.html', {
+                movies: movieArr
+            });
         })
         .catch(function (err) {
             console.log(err);
         });
 });
 
-// Servicio Post para crear una película
-app.post('/movie/add', function(req, res) {
+// Servicio Post para crear un nodo
+app.post('/movie/add', function (req, res) {
     var title = req.body.title;
     var released = req.body.released;
 
     session
-        .run('CREATE(n:Concept {title:{titleParam}, released:{yearParam}}) RETURN n.title', {titleParam: title, yearParam: released})
-        .then(function(result) {
+        .run('CREATE(n:Concept {title:{titleParam}, released:{yearParam}}) RETURN n.title', { titleParam: title, yearParam: released })
+        .then(function (result) {
             res.redirect('/');
             session.close();
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
         });
     res.redirect('/');
@@ -76,35 +61,40 @@ app.post('/movie/add', function(req, res) {
 
 
 // Servicio Post para relacionar dos conceptos
-app.post('/movie/actor/add', function(req, res) {
+app.post('/movie/actor/add', function (req, res) {
     var title1 = req.body.title1;
     var title2 = req.body.title2;
 
     session
-        .run('MATCH(a:Concept {title:{titleParam1}}),(b:Concept {title:{titleParam2}}) MERGE(a)-[r:CONCEPT]-(b) RETURN a,b', {titleParam1:title1, titleParam2: title2})
-        .then(function(result) {
+        .run('MATCH(a:Concept {title:{titleParam1}}),(b:Concept {title:{titleParam2}}) MERGE(a)-[r:CONCEPT]-(b) RETURN a,b', { titleParam1: title1, titleParam2: title2 })
+        .then(function (result) {
             res.redirect('/');
             session.close();
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
         });
     res.redirect('/');
 });
 
 // Servicio Post para encontrar el shortest path entre dos nodos
-app.post('/firstActor/secondActor/find', function(req, res) {
+app.post('/firstActor/secondActor/find', function (req, res) {
     var title1 = req.body.title1;
     var title2 = req.body.title2;
 
     session
-        .run('MATCH p=shortestPath((n:Concept {title: {firstActor}})-[*..4]-(m:Concept {title: {secondActor}})) RETURN p', {firstActor:title1, secondActor:title2})
-        .then(function(result) {
-            console.log(result.records[0]._fields);
+        .run('MATCH p=shortestPath((n:Concept {title: {firstActor}})-[*..4]->(m:Concept {title: {secondActor}})) RETURN p', { firstActor: title1, secondActor: title2 })
+        .then(function (result) {
+            var segmentos = result.records[0]._fields[0].segments;
+           for(var count = 0; count < segmentos.length; count++){
+               console.log(segmentos[count].start.properties.title);
+               console.log(segmentos[count].relationship.type);
+           }
+
             res.redirect('/');
             session.close();
         })
-        .catch(function(err) {
+        .catch(function (err) {
             console.log(err);
         });
     res.redirect('/');
