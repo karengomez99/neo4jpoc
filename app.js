61a6dae1450386985ee32fea0900831d79bd9ef9
mjpintoh <gomezsuarezk99@gmail.com>
2018-05-14 22:27:29 -0500

    miguel 14 mayo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 //View engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -18,37 +18,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Driver para la session de conexión con neo4j
-var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '12345')); // Conect neo4j
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'neo4j')); // Conect neo4j
 var session = driver.session();
 
-// Servicio get para obtener los nodos
-app.get('/', function (req, res) {
-    session
-        .run('MATCH(n:Concept) RETURN n')
-        .then(function (result) {
-            var movieArr = [];
-            result.records.forEach(function (record) {
-                movieArr.push({
-                    title: record._fields[0].properties.title,
-                    released: record._fields[0].properties.released
-                });
-            });
-            res.sendFile(path1 + 'poc.html', {
-                movies: movieArr
-            });
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
+app.get('/', function(req, res){
+    res.render('index');
 });
 
+app.get('/create', function(req, res){
+    res.render('create_node');
+});
+
+app.get('/createRelationship', function(req, res){
+    res.render('create_relationship');
+});
+
+
 // Servicio Post para crear un nodo
-app.post('/movie/add', function (req, res) {
-    var title = req.body.title;
-    var released = req.body.released;
+app.post('/createNode', function (req, res) {
+    var titleParam = req.body.nodeTitle;
+    var descriptionParam = req.body.nodeDescription;
 
     session
-        .run('CREATE(n:Concept {title:{titleParam}, released:{yearParam}}) RETURN n.title', { titleParam: title, yearParam: released })
+        .run('CREATE(n:Nodes {nodeTitle:{nodeTitleParam}, nodeDescription:{nodeDescriptionParam}}) RETURN n.nodeTitle', { nodeTitleParam: titleParam, nodeDescriptionParam: descriptionParam })
         .then(function (result) {
             res.redirect('/');
             session.close();
@@ -56,17 +48,19 @@ app.post('/movie/add', function (req, res) {
         .catch(function (err) {
             console.log(err);
         });
-    res.redirect('/');
+  //  res.redirect('/');
 });
 
 
 // Servicio Post para relacionar dos conceptos
-app.post('/movie/actor/add', function (req, res) {
-    var title1 = req.body.title1;
-    var title2 = req.body.title2;
+app.post('/createRelation', function (req, res) {
+    var nodeFrom = req.body.nodeFrom;
+    var nodeTo = req.body.nodeTo;
+    var optionVal = req.body.optionVal;
 
-    session
-        .run('MATCH(a:Concept {title:{titleParam1}}),(b:Concept {title:{titleParam2}}) MERGE(a)-[r:CONCEPT]-(b) RETURN a,b', { titleParam1: title1, titleParam2: title2 })
+    console.log(optionVal);
+   /* session
+        .run('MATCH(a:Nodes {nodeTitle:{titleParam1}}),(b:Nodes {nodeTitle:{titleParam2}}) MERGE(a)-[r:CONCEPT]-(b) RETURN a,b', { titleParam1: nodeFrom, titleParam2: nodeTo, optionVal: optionVal })
         .then(function (result) {
             res.redirect('/');
             session.close();
@@ -74,10 +68,10 @@ app.post('/movie/actor/add', function (req, res) {
         .catch(function (err) {
             console.log(err);
         });
-    res.redirect('/');
+    res.redirect('/');*/
 });
 
-// Servicio Post para encontrar el shortest path entre dos nodos
+/* Servicio Post para encontrar el shortest path entre dos nodos
 app.post('/firstActor/secondActor/find', function (req, res) {
     var title1 = req.body.title1;
     var title2 = req.body.title2;
@@ -85,12 +79,31 @@ app.post('/firstActor/secondActor/find', function (req, res) {
     session
         .run('MATCH p=shortestPath((n:Concept {title: {firstActor}})-[*..4]->(m:Concept {title: {secondActor}})) RETURN p', { firstActor: title1, secondActor: title2 })
         .then(function (result) {
-            var segmentos = result.records[0]._fields[0].segments;
+            var segmentos = result.records[0]._fields[0].segments[0].start.properties.title;
+
+            res.render('index1', { title0: segmentos });
+            
            for(var count = 0; count < segmentos.length; count++){
                console.log(segmentos[count].start.properties.title);
                console.log(segmentos[count].relationship.type);
            }
 
+         //   res.redirect('/');
+            session.close();
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+ //   res.redirect('/');
+});*/
+
+/* Servicio Post para borrar base de datos
+app.post('/borrarbd', function (req, res) {
+  
+    session
+        .run('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+        .then(function (result) {
+            console.log('Bd borrada');
             res.redirect('/');
             session.close();
         })
@@ -98,8 +111,7 @@ app.post('/firstActor/secondActor/find', function (req, res) {
             console.log(err);
         });
     res.redirect('/');
-});
-
+});*/
 
 app.listen(3000);
 console.log('Server Started on Port 3000');
